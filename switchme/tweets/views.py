b0da53f97817aa959f3950381abdc107270fdf9d
nikit34 <permikov134@mail.ru>
2020-07-25 01:12:27 +0500

    add check status code

--- switchme/tweets/views.py
+++ switchme/tweets/views.py
@@ -20,20 +20,22 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201)
         if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={'form': form})
 
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{
-        'id': x.id,
-        'content': x.content,
-        'likes': 0
-    } for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
+        'isUser': False,
         'response': tweets_list
     }
     return JsonResponse(data)
