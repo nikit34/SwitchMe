022042ac7a9a23f414f874f60a4bec33e536f72a
nikit34 <permikov134@mail.ru>
2020-07-24 19:48:54 +0500

    redirect forms unput tweets

--- switchme/tweets/views.py
+++ switchme/tweets/views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import TweetForm
 from .models import Tweet
@@ -11,9 +11,12 @@ def home_view(request, *args, **kwargs):
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if next_url is not None:
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={'form': form})
 
