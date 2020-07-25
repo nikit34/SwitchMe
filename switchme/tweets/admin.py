1f73434c3472f6472c35bf5a63eec0a55c89e197
nikit34 <permikov134@mail.ru>
2020-07-25 07:08:28 +0500

    add user default accs

--- switchme/tweets/admin.py
+++ switchme/tweets/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tweet
+
+
+class TweetAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'user__email']
+
+    class Meta:
+        model = Tweet
+
+
+admin.site.register(Tweet)
