1c34b96e3192c04c6e9afd24135826f45d2d2995
nikit34 <permikov134@mail.ru>
2020-07-26 21:23:28 +0500

    add rest authentication

--- switchme/tweets/views.py
+++ switchme/tweets/views.py
@@ -2,8 +2,10 @@ from django.http import JsonResponse
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from .forms import TweetForm
 from .models import Tweet
@@ -18,6 +20,8 @@ def home_view(request, *args, **kwargs):
 
 
 @api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
