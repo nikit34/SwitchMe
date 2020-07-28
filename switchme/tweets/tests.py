0e28079e355ecc9f9f5527ab108354358de82e3f
nikit34 <permikov134@mail.ru>
2020-07-28 08:46:00 +0500

    add tests - one dont pass

--- switchme/tweets/tests.py
+++ switchme/tweets/tests.py
@@ -1,3 +1,51 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Tweet
+
+
+User = get_user_model()
+
+
+class TweetTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='somepassword')
+        Tweet.objects.create(content='auto tests\n first content', user=self.user)
+        Tweet.objects.create(content='auto tests\n first content', user=self.user)
+        Tweet.objects.create(content='auto tests\n first content', user=self.user)
+
+    def test_user_created(self):
+        self.assertEqual(self.user.username, 'test')
+
+    def test_tweet_created(self):
+        tweet_obj = Tweet.objects.create(content='auto tests\n second content', user=self.user)
+        self.assertEqual(tweet_obj.id, 4)
+        self.assertEqual(tweet_obj.user, self.user)
+
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='somepasswordapi')
+        return client
+
+    def test_tweet_list(self):
+        client = self.get_client()
+        response = client.get('/api/tweets/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 3)
+
+    def test_action_like(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', {'id': 1, 'action': 'like'})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get('likes')
+        self.assertEqual(len(like_count), 1)
+
+    def test_action_unlike(self):
+        client = self.get_client()
+        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'like'})
+        self.assertEqual(response.status_code, 200)
+        response = client.post('/api/tweets/action/', {'id': 2, 'action': 'unlike'})
+        self.assertEqual(response.status_code, 200)
+        like_count = response.json().get('likes')
+        self.assertEqual(len(like_count), 0)
--- switchme/tweets/views.py
+++ switchme/tweets/views.py
@@ -1,5 +1,5 @@
-from django.http import JsonResponse
 from django.conf import settings
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from rest_framework.authentication import SessionAuthentication
@@ -7,8 +7,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from .forms import TweetForm
 from .models import Tweet
+from .forms import TweetForm
 from .serializers import (
     TweetSerializer,
     TweetCreateSerializer,
@@ -23,7 +23,7 @@ def home_view(request, *args, **kwargs):
     username = None
     if request.user.is_authenticated:
         username = request.user.username
-    return render(request, 'pages/home.html', context={}, status=200)
+    return render(request, 'pages/home.html', context={'username': username}, status=200)
 
 
 @api_view(['POST'])
@@ -87,6 +87,8 @@ def tweet_action_view(request, *args, **kwargs):
             return Response(serializer.data, status=200)
         elif action == 'unlike':
             obj.likes.remove(request.user)
+            serializer = TweetSerializer(obj)
+            return Response(serializer.data, status=200)
         elif action == 'retweet':
             new_tweet = Tweet.objects.create(
                 user=request.user,
@@ -94,54 +96,5 @@ def tweet_action_view(request, *args, **kwargs):
                 content=content,
             )
             serializer = TweetSerializer(new_tweet)
-            return Response(serializer.data, status=200)
+            return Response(serializer.data, status=201)
     return Response({}, status=200)
-
-
-def tweet_create_view_pure_django(request, *args, **kwargs):
-    user = request.user
-    if not request.user.is_authenticated:
-        user = None
-        if request.is_ajax():
-            return JsonResponse({}, status=401)
-        return redirect(settings.LOGIN_URL)
-    form = TweetForm(request.POST or None)
-    next_url = request.POST.get('next') or None
-    if form.is_valid():
-        obj = form.save(commit=False)
-        obj.user = user
-        obj.save()
-        if request.is_ajax():
-            return JsonResponse(obj.serialize(), status=201)
-        if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
-            return redirect(next_url)
-        form = TweetForm()
-    if form.errors:
-        if request.is_ajax():
-            return JsonResponse(form.errors, status=400)
-    return render(request, 'components/form.html', context={'form': form})
-
-
-def tweet_list_view_pure_django(request, *args, **kwargs):
-    qs = Tweet.objects.all()
-    tweets_list = [x.serialize() for x in qs]
-    data = {
-        'isUser': False,
-        'response': tweets_list
-    }
-    return JsonResponse(data)
-
-
-def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
-    data = {
-        'id': tweet_id,
-    }
-    status = 200
-    try:
-        obj = Tweet.objects.get(id=tweet_id)
-        data['content'] = obj.content
-    except:
-        data['message'] = 'Not Found'
-        status = 404
-    return JsonResponse(data, status=status)
-
