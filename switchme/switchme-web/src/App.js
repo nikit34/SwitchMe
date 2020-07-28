f849ec7d3a4bfbec154627d3c4dc6e2964b5100b
nikit34 <permikov134@mail.ru>
2020-07-28 22:19:26 +0500

    add btn component

--- switchme/switchme-web/src/App.js
+++ switchme/switchme-web/src/App.js
@@ -19,13 +19,30 @@ function loadTweets(callback) {
     xhr.send()
 }
 
+function ActionBtn(props) {
+    const {tweet, action} = props
+    const className = props.className ? props.className : 'btn btn-primary btn-sm'
+    return action.type === 'like' ? <button className={className}>{tweet.likes} Like</button> : null
+}
+
+function Tweet(props) {
+    const {tweet} = props
+    const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
+    return <div className={className}>
+        <p>{tweet.id} - {tweet.content}</p>
+        <div className='btn btn-group'>
+            <ActionBtn tweet={tweet} action={{type: "like"}}/>
+            <ActionBtn tweet={tweet} action={{type: "unlike"}}/>
+        </div>
+    </div>
+}
+
 
 function App() {
     const [tweets, setTweets] = useState([])
 
     useEffect(() => {
         const myCallback = (response, status) => {
-            console.log(response, status)
             if(status === 200){
                 setTweets(response)
             }
@@ -39,11 +56,11 @@ function App() {
                 <p>
                   Edit <code>src/App.js</code> and save to reload.
                 </p>
-                <p>
-                  {tweets.map((tweet, index)=>{
-                    return <li>{tweet.content}</li>
+                <div>
+                  {tweets.map((item, index)=>{
+                    return <Tweet tweet={item} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
                   })}
-                </p>
+                </div>
                 <a
                   className="App-link"
                   href="https://reactjs.org"
