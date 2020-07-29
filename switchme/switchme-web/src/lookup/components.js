77627833614b51028385c808ddb1aef2d1d13470
nikit34 <permikov134@mail.ru>
2020-07-29 14:55:21 +0500

    need integrate auth

--- switchme/switchme-web/src/lookup/components.js
+++ switchme/switchme-web/src/lookup/components.js
@@ -1,3 +1,18 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 function lookup(method, endpoint, callback, data) {
     let jsonData;
     if(data){
@@ -6,7 +21,14 @@ function lookup(method, endpoint, callback, data) {
     const xhr = new XMLHttpRequest()
     const url = `http://localhost:8000/api${endpoint}`
     xhr.responseType = 'json'
+    const csrftoken = getCookie('csrftoken')
     xhr.open(method, url)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    if(csrftoken){
+        xhr.setRequestHeader('HTTP_X_REQUESTED_WITH', 'XMLHttpRequest')
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+        xhr.setRequestHeader('X-CSRFToken', csrftoken)
+    }
     xhr.onload = function () {
         callback(xhr.response, xhr.status)
     }
@@ -16,6 +38,11 @@ function lookup(method, endpoint, callback, data) {
     }
     xhr.send(jsonData)
 }
+
+export function createTweet(newTweet, callback) {
+    lookup("POST", "/tweets/create/", callback, {content: newTweet})
+}
+
 export function loadTweets(callback) {
     lookup("GET", "/tweets/", callback)
 }
