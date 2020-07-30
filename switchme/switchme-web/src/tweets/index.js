ffa33fa0e3d373c4e151cd93596b0e83f6085c5b
nikit34 <permikov134@mail.ru>
2020-07-30 22:39:55 +0500

    separate js files

--- switchme/switchme-web/src/tweets/index.js
+++ switchme/switchme-web/src/tweets/index.js
@@ -1,8 +1,14 @@
-import {ActionBtn, Tweet, TweetsList, TweetsComponent} from "./components";
+import {TweetsComponent} from "./components";
+import {ActionBtn} from "./buttons";
+import {Tweet} from "./detail";
+import {TweetsList} from "./list";
+import {TweetCreate} from "./create";
+
 
 export {
     ActionBtn,
     Tweet,
     TweetsList,
-    TweetsComponent
+    TweetsComponent,
+    TweetCreate
 }
