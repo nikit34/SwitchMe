f6f746e169d530b8c112c658cb153ee90a73f48e
nikit34 <permikov134@mail.ru>
2020-07-30 10:52:49 +0500

    add data attr for root

--- switchme/switchme-web/src/index.js
+++ switchme/switchme-web/src/index.js
@@ -10,9 +10,11 @@ const appEl = document.getElementById('root')
 if(appEl){
     ReactDOM.render(<App />, appEl);
 }
+
+const e = React.createElement
 const tweetsEl = document.getElementById("switchme-2")
 if(tweetsEl){
-    ReactDOM.render(<TweetsComponent />, tweetsEl)
+    ReactDOM.render(e(TweetsComponent, tweetsEl.dataset), tweetsEl)
 }
 
 
