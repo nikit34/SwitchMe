c3b7ee6e2add7f2d87d8d0d4a7cec58bec4e24b2
nikit34 <permikov134@mail.ru>
2020-07-31 07:58:00 +0500

    ref models

--- switchme/tweets/models.py
+++ switchme/tweets/models.py
@@ -8,12 +8,12 @@ User = settings.AUTH_USER_MODEL
 
 class TweetLike(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    tweet = models.ForeignKey('Tweet', on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
 class Tweet(models.Model):
-    parent = models.ForeignKey('self', null=True, on_delete=models.SET_NULL)
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
