45db37ccd3fc4cf1e0863db2fc3b0d14f7996e3c
nikit34 <permikov134@mail.ru>
2020-07-31 14:53:05 +0500

    add post save profile

--- switchme/profiles/views.py
+++ switchme/profiles/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Profile
 
 
 def profile_detail_view(request, username, *args, **kwargs):
-    return render(request, 'profiles/detail.html', {'username': username})
+    qs = Profile.objects.filter(user__username=username)
+    if not qs.exists():
+        raise Http404
+    profile_obj = qs.first()
+    context = {
+        'username': username,
+        'profile': profile_obj
+    }
+    return render(request, 'profiles/detail.html', context)
