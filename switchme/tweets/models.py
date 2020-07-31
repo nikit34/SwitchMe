a73e989a9970bbe9ccab40854af7b8f9a6544bf6
nikit34 <permikov134@mail.ru>
2020-07-31 08:58:32 +0500

    eee!!! fix big boss bugs

--- switchme/tweets/models.py
+++ switchme/tweets/models.py
@@ -13,7 +13,7 @@ class TweetLike(models.Model):
 
 
 class Tweet(models.Model):
-    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+    parent = models.ForeignKey("self", blank=True, null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='tweet_user', blank=True, through=TweetLike)
     content = models.TextField(blank=True, null=True)
