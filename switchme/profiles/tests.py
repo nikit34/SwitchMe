64804669d3358db0dd80af4afbc07e7f0152ab7e
nikit34 <permikov134@mail.ru>
2020-07-31 21:23:06 +0500

    add tests profile

--- switchme/profiles/tests.py
+++ switchme/profiles/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from .models import Profile
+
+
+User = get_user_model()
+
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test', password='somepassword')
+        self.user2 = User.objects.create_user(username='test2', password='somepassword2')
+
+    def test_profile_created(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.user2
+        first.profile.followers.add(second)
+        second_user_following_whom = second.following.all()
+        qs = second_user_following_whom.filter(user=first)
+        first_user_following_no_one = first.following.all()
+        self.assertTrue(qs.exists())
+        self.assertFalse(first_user_following_no_one.exists())
