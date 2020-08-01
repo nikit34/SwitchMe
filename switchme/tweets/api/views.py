eb8087cd61c9f1a64a97bb37bca029ebfb90a7ae
nikit34 <permikov134@mail.ru>
2020-08-01 06:33:26 +0500

    add paginate

--- switchme/tweets/api/views.py
+++ switchme/tweets/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from ..models import Tweet
 from ..serializers import (
@@ -28,8 +29,15 @@ def tweet_list_view(request, *args, **kwargs):
     username = request.GET.get('username')
     if username is not None:
         qs = qs.by_username(username)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    return get_paginated_queryset_response(qs, request)
+
+
+def get_paginated_queryset_response(qs, request):
+    paginator = PageNumberPagination()
+    paginator.page_size = 5
+    paginator_qs = paginator.paginate_queryset(qs, request)
+    serializer = TweetSerializer(paginator_qs, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 
 @api_view(['GET'])
@@ -37,8 +45,7 @@ def tweet_list_view(request, *args, **kwargs):
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
     qs = Tweet.objects.feed(user)
-    serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data, status=200)
+    return get_paginated_queryset_response(qs, request)
 
 
 @api_view(['GET'])
