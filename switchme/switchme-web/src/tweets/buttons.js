822e14d187e0b8fb8b67156affb543ff5298f7f6
nikit34 <permikov134@mail.ru>
2020-08-01 20:04:57 +0500

    delete logs js

--- switchme/switchme-web/src/tweets/buttons.js
+++ switchme/switchme-web/src/tweets/buttons.js
@@ -8,7 +8,6 @@ export function ActionBtn(props) {
     const className = props.className ? props.className : 'btn btn-primary btn-sm'
     const actionDisplay = action.display ? action.display : 'Action'
     const handleActionBackendEvent = (response, status) => {
-        console.log(response, status)
         if ((status === 200 || status === 201) && didPerformAction) {
             didPerformAction(response, status)
         }
