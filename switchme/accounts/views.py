aa28a4bf51495c189a22fd0eff02ae50fcd3ffc8
nikit34 <permikov134@mail.ru>
2020-08-02 08:00:30 +0500

    correct

--- switchme/accounts/views.py
+++ switchme/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 
 
 def login_view(request, *args, **kwargs):
