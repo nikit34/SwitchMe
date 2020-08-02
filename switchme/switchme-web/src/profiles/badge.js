8723e13a84012123560e64852e1fcad0e2e0efa9
nikit34 <permikov134@mail.ru>
2020-08-02 15:08:00 +0500

    add badge toogle

--- switchme/switchme-web/src/profiles/badge.js
+++ switchme/switchme-web/src/profiles/badge.js
@@ -1,11 +1,30 @@
 import React, {useEffect, useState} from 'react'
-import {apiProfileDetail} from './lookup';
+import {apiProfileDetail, apiProfileFollowToggle} from './lookup';
+import {UserDisplay, UserPicture} from "./components";
 
+
+function ProfileBadge(props){
+    const {user, didFollowToggle, profileLoading} = props
+    let currentVerb = (user && user.is_following) ? "Unfollow": "Follow"
+    currentVerb = profileLoading ? "Loading..." : currentVerb
+    const handleFollowToggle = (event) => {
+        event.preventDefault()
+        if(didFollowToggle && !profileLoading){
+            didFollowToggle(currentVerb)
+        }
+    }
+    return user ? <div>
+        <UserPicture user={user} hideLink />
+        <p><UserDisplay user={user} includeFullName hideLink/></p>
+        <button className='btn btn-primary' onClick={handleFollowToggle}>{currentVerb}</button>
+    </div> : null
+}
 
 export function ProfileBadgeComponent(props){
     const {username} = props
     const [didLookup, setDidLookup] = useState(false)
     const [profile, setProfile] = useState(null)
+    const [profileLoading, setProfileLoading] = useState(null)
     const handleBackendLookup = (response, status) => {
         if(status === 200){
             setProfile(response)
@@ -17,5 +36,15 @@ export function ProfileBadgeComponent(props){
             setDidLookup(true)
         }
     }, [username, didLookup, setDidLookup])
-    return didLookup === false ? "Loadinf..." : profile ? <span>{profile.first_name}</span> : null
+    const handleNewFollow = (actionVerb) => {
+        apiProfileFollowToggle(username, actionVerb, (response, status) => {
+            if(status === 200){
+                apiProfileDetail(username, handleBackendLookup)
+                setProfile(response)
+            }
+            setProfileLoading(false)
+        })
+        setProfileLoading(true)
+    }
+    return didLookup === false ? "Loading..." : profile ? <ProfileBadge user={profile} didFollowToggle={handleNewFollow} profileLeading={profileLoading} /> : null
 }
