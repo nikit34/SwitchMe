648caca08a1f0ea18709377f2b21f2d89d538912
nikit34 <permikov134@mail.ru>
2020-08-02 11:35:10 +0500

    add budge views

---
+++ switchme/switchme-web/src/profiles/badge.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react'
+import {apiProfileDetail} from './lookup';
+
+
+export function ProfileBadgeComponent(props){
+    const {username} = props
+    const [didLookup, setDidLookup] = useState(false)
+    const [profile, setProfile] = useState(null)
+    const handleBackendLookup = (response, status) => {
+        if(status === 200){
+            setProfile(response)
+        }
+    }
+    useEffect(() => {
+        if(didLookup === false){
+            apiProfileDetail(username, handleBackendLookup)
+            setDidLookup(true)
+        }
+    }, [username, didLookup, setDidLookup])
+    return didLookup === false ? "Loadinf..." : profile ? <span>{profile.first_name}</span> : null
+}
--- switchme/switchme-web/src/profiles/index.js
+++ switchme/switchme-web/src/profiles/index.js
@@ -1,7 +1,9 @@
 import {UserDisplay, UserLink, UserPicture} from "./components";
+import {ProfileBadgeComponent} from "./badge";
 
 
 export {
+    ProfileBadgeComponent,
     UserPicture,
     UserLink,
     UserDisplay
