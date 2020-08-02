7ac089709a3db0ba686cf010c05ca56fc0beeb1a
nikit34 <permikov134@mail.ru>
2020-08-02 18:30:39 +0500

    add view info

---
+++ switchme/switchme-web/src/profiles/utils.js
@@ -0,0 +1,7 @@
+import numeral from "numeral";
+import React from "react";
+
+
+export function DisplayCount(props) {
+    return <span className={props.className}>{numeral(props.children).format('0a')}</span>
+}
